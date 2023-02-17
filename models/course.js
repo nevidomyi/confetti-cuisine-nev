df47925493f580bbd886e238bf3b7fe19cc68e04
Ruslan Mamedov <47025875+nevidomyi@users.noreply.github.com>
2023-02-17 10:43:12 +0200

    Try fixxing course modal

--- models/course.js
+++ models/course.js
@@ -1,28 +1,33 @@
+"use strict";
+
 const mongoose = require("mongoose"),
-    { Schema } = require("mongoose"),
-    courseSchema = new Schema({
+  { Schema } = require("mongoose");
+
+var courseSchema = new Schema(
+  {
     title: {
-        type: String,
-        require: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true
     },
     description: {
-        type: String,
-        require: true,
+      type: String,
+      required: true
     },
     maxStudents: {
-        type: Number,
-        default: 0,
-        min: [0, "Course cannot have a negative number of students"]
+      type: Number,
+      default: 0,
+      min: [0, "Course cannot have a negative number of students"]
     },
     cost: {
-        type: Number,
-        default: 0,
-        min: [0, "Course cannot have a negative cost"]
+      type: Number,
+      default: 0,
+      min: [0, "Course cannot have a negative cost"]
     }
-},
-{
+  },
+  {
     timestamps: true
-}
+  }
 );
+
 module.exports = mongoose.model("Course", courseSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,15 @@
-const usersController = require("../controllers/usersController");
+"use strict";
 
 const router = require("express").Router(),
-    coursesController = require("../controllers/coursesController"),
-    userConroller = require("../controllers/usersController");
+  coursesController = require("../controllers/coursesController");
 
-router.use(usersController.verifyJWT);
-router.post("/login", usersController.apiAuthenticate);
-router.use(coursesController.errorJSON);
-router.get("/courses", coursesController.index, coursesController.filterUserCourses, coursesController.respondJSON);
+router.get(
+  "/courses",
+  coursesController.index,
+  coursesController.filterUserCourses,
+  coursesController.respondJSON
+);
 router.get("/courses/:id/join", coursesController.join, coursesController.respondJSON);
+router.use(coursesController.errorJSON);
 
 module.exports = router;
